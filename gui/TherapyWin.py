e56f65c270da9d745571606d867b0af4e4fa5263
Nathalia Cespedes <nathalia.cespedes@mail.escuelaing.edu.co>
2019-10-14 15:43:43 +0200

    TherapyWin added

--- gui/TherapyWin.py
+++ gui/TherapyWin.py
@@ -31,19 +31,124 @@ class TherapyWindow(QtGui.QMainWindow):
         self.label_background=QtGui.QLabel(self)
         self.label_background.setGeometry(QtCore.QRect(0,0,self.winsize_h,self.winsize_v))
         self.label_background.setScaledContents(True)
-        self.label_background.setPixmap(QtGui.QPixmap("gui/img/Back.png"))
+        self.label_background.setPixmap(QtGui.QPixmap("img/Back.png"))
 
         #Setting Close Image 
         self.close1=QtGui.QLabel(self)
         self.close1.setGeometry(QtCore.QRect(self.winsize_h*0.93,self.winsize_v*0.05,self.winsize_h*0.045,self.winsize_h*0.045))
-        Icon1=QtGui.QPixmap("gui/img/closebtn.PNG")
+        Icon1=QtGui.QPixmap("img/closebtn.PNG")
         Icon_resize1= Icon1.scaled(self.winsize_h*0.045,self.winsize_h*0.045,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
         self.close1.setPixmap(Icon_resize1)
 
+        #Setting HR Image
+
+        self.heart=QtGui.QLabel(self)
+        self.heart.setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.33,self.winsize_h*0.04,self.winsize_h*0.04))
+        heart=QtGui.QPixmap("img/heart.PNG")
+        heart= heart.scaled(self.winsize_h*0.04,self.winsize_h*0.04,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.heart.setPixmap(heart)
+
+        self.heart_label=QtGui.QLabel(self)
+        self.heart_label.setGeometry(QtCore.QRect(self.winsize_h*0.24,self.winsize_v*0.34,self.winsize_h*0.25,self.winsize_h*0.03))
+        heart_label=QtGui.QPixmap("img/rect5562.PNG")
+        heart_label= heart_label.scaled(self.winsize_h*0.3,self.winsize_h*0.04,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.heart_label.setPixmap(heart_label)
+
+        #Setting posture Image
+
+        self.posture=QtGui.QLabel(self)
+        self.posture.setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.43,self.winsize_h*0.04,self.winsize_h*0.04))
+        posture =QtGui.QPixmap("img/posture.PNG")
+        posture = posture.scaled(self.winsize_h*0.04,self.winsize_h*0.04,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.posture.setPixmap(posture)
+
+        self.posture_label=QtGui.QLabel(self)
+        self.posture_label.setGeometry(QtCore.QRect(self.winsize_h*0.24,self.winsize_v*0.44,self.winsize_h*0.25,self.winsize_h*0.03))
+        posture_label=QtGui.QPixmap("img/rect5562.PNG")
+        posture_label= heart_label.scaled(self.winsize_h*0.3,self.winsize_h*0.04,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.posture_label.setPixmap(posture_label)
+
+        # Settings patient name
+        self.Patient = {}
+        self.Patient['name'] = QtGui.QLabel(self)
+        self.Patient['name'].setText("Nombre del paciente :")
+        self.Patient['name'].setStyleSheet("font-size:30px; Arial")
+        self.Patient['name'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.90,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        self.Patient = {}
+        self.Patient['id'] = QtGui.QLabel(self)
+        self.Patient['id'].setText("ID del paciente :")
+        self.Patient['id'].setStyleSheet("font-size:30px; Arial")
+        self.Patient['id'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.93,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        
+        #Setting priority texts
+        self.PriorityText = {}
+        self.PriorityText['name'] = QtGui.QLabel(self)
+        self.PriorityText['name'].setText("Ingresa prioridad de realimentacion :")
+        self.PriorityText['name'].setStyleSheet("font-size:40px; Arial")
+        self.PriorityText['name'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.1,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        self.Options = {}
+        self.Options['name_first'] = QtGui.QLabel(self)
+        self.Options['name_first'].setText("1:")
+        self.Options['name_first'].setStyleSheet("font-size:40px; Arial")
+        self.Options['name_first'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.15,self.winsize_h*0.05 ,self.winsize_h*0.02))
+
+        self.Options = {}
+        self.Options['name_second'] = QtGui.QLabel(self)
+        self.Options['name_second'].setText("2:")
+        self.Options['name_second'].setStyleSheet("font-size:40px; Arial")
+        self.Options['name_second'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.20,self.winsize_h*0.05 ,self.winsize_h*0.02))
+
+        #Priority Options
+        self.Options['read_first'] = QtGui.QComboBox(self)
+        self.Options['read_first'].setStyleSheet("font-size:35px; Arial")
+        self.Options['read_first'].setGeometry(QtCore.QRect(self.winsize_h*0.067,self.winsize_v*0.15,self.winsize_h*0.15 ,self.winsize_h*0.02))   
+        self.Options['read_first'].addItem("EMG")
+        self.Options['read_first'].addItem("Postura") 
+
+        self.Options['read_second'] = QtGui.QComboBox(self)
+        self.Options['read_second'].setStyleSheet("font-size:35px; Arial")
+        self.Options['read_second'].setGeometry(QtCore.QRect(self.winsize_h*0.067,self.winsize_v*0.20,self.winsize_h*0.15 ,self.winsize_h*0.02))   
+        self.Options['read_second'].addItem("Postura")
+        self.Options['read_second'].addItem("EMG") 
+
+        # Sensor labels and text
+
+        self.ECG = {}
+        self.ECG['name'] = QtGui.QLabel(self)
+        self.ECG['name'].setText("Ritmo Cardiaco:")
+        self.ECG['name'].setStyleSheet("font-size:40px; Arial")
+        self.ECG['name'].setGeometry(QtCore.QRect(self.winsize_h*0.1,self.winsize_v*0.35,self.winsize_h*0.3 ,self.winsize_h*0.02))
+
+        self.ECG['lcd'] = QtGui.QLCDNumber(self)
+        self.ECG['lcd'].setGeometry(QtCore.QRect(self.winsize_h*0.26,self.winsize_v*0.34,self.winsize_h*0.1 ,self.winsize_h*0.03))
+
+        self.Inclination = {}
+        self.Inclination['name'] = QtGui.QLabel(self)
+        self.Inclination['name'].setText("Grado de inclinacion:")
+        self.Inclination['name'].setStyleSheet("font-size:40px; Arial")
+        self.Inclination['name'].setGeometry(QtCore.QRect(self.winsize_h*0.1,self.winsize_v*0.45,self.winsize_h*0.3 ,self.winsize_h*0.02))
+
+        self.Inclination['lcd'] = QtGui.QLCDNumber(self)
+        self.Inclination['lcd'].setGeometry(QtCore.QRect(self.winsize_h*0.26,self.winsize_v*0.44,self.winsize_h*0.1 ,self.winsize_h*0.03))
+
+        # Setting the buttons of the interface:
+        self.controlButtons={}
+        #Close Button
+        self.controlButtons['close'] = QtGui.QCommandLinkButton(self)
+        self.controlButtons['close'].setIconSize(QSize(0,0))
+        self.controlButtons['close'].setGeometry(QtCore.QRect(self.winsize_h*0.93,self.winsize_v*0.05,self.winsize_h*0.045,self.winsize_h*0.045)) 
+        
+
+
        
+        self.show()
+        self.setPatientName("Nathalia Cespedes Gomez", "1026295355")
 
         self.connectCloseButton();
-        self.connectVolumeSlider(self.get_Volume)
+        
 
     def connectSpeechButton(self,f):
         self.controlButtons['speech'].clicked.connect(f) 
@@ -52,42 +157,31 @@ class TherapyWindow(QtGui.QMainWindow):
         self.controlButtons['close'].clicked.connect(self.close)
         self.onClose.emit()
 
-    def connectVolumeSlider(self,f):
-        self.volume.valueChanged.connect(f)
+    def setPatientName(self, value, id_patient):
 
-    def get_Volume(self):
-        self.volume_data = self.volume.value()
-        self.VolumeText['lcd'].display(self.volume_data)
+        value =str(value) 
+        self.SetPatient = {}
+        self.SetPatient['name'] = QtGui.QLabel(self)
+        self.SetPatient['name'].setText(value)
+        self.SetPatient['name'].setStyleSheet("font-size:30px; Arial")
+        self.SetPatient['name'].setGeometry(QtCore.QRect(self.winsize_h*0.18,self.winsize_v*0.90,self.winsize_h*0.25 ,self.winsize_h*0.02))
+        self.SetPatient['name'].show()
 
-    def send_Volume(self):
-        self.volume_data = self.volume.value()
-        return(self.volume_data)
+        id_patient =str(id_patient)
+        self.SetPatient['id'] = QtGui.QLabel(self)
+        self.SetPatient['id'].setText(id_patient)
+        self.SetPatient['id'].setStyleSheet("font-size:30px; Arial")
+        self.SetPatient['id'].setGeometry(QtCore.QRect(self.winsize_h*0.18,self.winsize_v*0.93,self.winsize_h*0.25 ,self.winsize_h*0.02))
+        self.SetPatient['id'].show()
 
-    def connectElephantButton(self,f): 
-        self.controlButtons['Elephant'].clicked.connect(f) 
 
-    def connectMouseButton(self,f): 
-        self.controlButtons['Mouse'].clicked.connect(f)
 
-    def connectButterflyButton(self,f): 
-        self.controlButtons['butterfly'].clicked.connect(f)
-
-    def connectMonkeyButton(self,f): 
-        self.controlButtons['monkey'].clicked.connect(f)
-
-    def get_speech(self):
-
-        speech  = str(self.SpeechText['read'].text())
-        return(speech)
-
-    def connectRightButton(self,f):
-        self.controlButtons['yes'].clicked.connect(f)
 
 
 def test():
     app=QtGui.QApplication(sys.argv)
-    GUI=PlayWindow()
+    GUI=TherapyWindow()
     sys.exit(app.exec_())
-#A=test()       
+A=test()       
 
 
