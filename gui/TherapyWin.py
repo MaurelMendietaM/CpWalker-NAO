1c7ed5c19c7733891989fedcd87e016d10d13758
Nathalia Cespedes <nathalia.cespedes@mail.escuelaing.edu.co>
2019-10-15 12:10:31 +0200

    Therapy interface updated

--- gui/TherapyWin.py
+++ gui/TherapyWin.py
@@ -4,6 +4,8 @@ from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import*
 from PyQt4.QtGui import*
 import ctypes
+import pyqtgraph
+from pyqtgraph import PlotWidget
 
 class TherapyWindow(QtGui.QMainWindow):
 
@@ -40,6 +42,51 @@ class TherapyWindow(QtGui.QMainWindow):
         Icon_resize1= Icon1.scaled(self.winsize_h*0.045,self.winsize_h*0.045,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
         self.close1.setPixmap(Icon_resize1)
 
+        #Setting Robot conexions Image
+        self.robot =QtGui.QLabel(self)
+        self.robot.setGeometry(QtCore.QRect(self.winsize_h*0.1,self.winsize_v*0.57,self.winsize_h*0.07,self.winsize_h*0.07))
+        robot=QtGui.QPixmap("img/Nao_head.PNG")
+        robot= robot.scaled(self.winsize_h*0.07,self.winsize_h*0.07,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.robot.setPixmap(robot)
+
+        #Setting Sensors conexion Image
+
+        self.Sensors =QtGui.QLabel(self)
+        self.Sensors.setGeometry(QtCore.QRect(self.winsize_h*0.1,self.winsize_v*0.69,self.winsize_h*0.07,self.winsize_h*0.07))
+        Sensors=QtGui.QPixmap("img/conex.PNG")
+        Sensors= Sensors.scaled(self.winsize_h*0.07,self.winsize_h*0.07,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.Sensors.setPixmap(Sensors)
+
+        self.Sensorsc = {}
+        self.Sensorsc['ECG'] = QtGui.QLabel(self)
+        self.Sensorsc['ECG'].setText("ECG:")
+        self.Sensorsc['ECG'].setStyleSheet("font-size:30px; Arial")
+        self.Sensorsc['ECG'].setGeometry(QtCore.QRect(self.winsize_h*0.19,self.winsize_v*0.7,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        
+        self.Sensorsc['IMU'] = QtGui.QLabel(self)
+        self.Sensorsc['IMU'].setText("IMU:")
+        self.Sensorsc['IMU'].setStyleSheet("font-size:30px; Arial")
+        self.Sensorsc['IMU'].setGeometry(QtCore.QRect(self.winsize_h*0.19,self.winsize_v*0.73,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        self.Sensorsc['EMG'] = QtGui.QLabel(self)
+        self.Sensorsc['EMG'].setText("EMG:")
+        self.Sensorsc['EMG'].setStyleSheet("font-size:30px; Arial")
+        self.Sensorsc['EMG'].setGeometry(QtCore.QRect(self.winsize_h*0.19,self.winsize_v*0.76,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        self.Sensorsc['robot'] = QtGui.QLabel(self)
+        self.Sensorsc['robot'].setText("Ip:")
+        self.Sensorsc['robot'].setStyleSheet("font-size:30px; Arial")
+        self.Sensorsc['robot'].setGeometry(QtCore.QRect(self.winsize_h*0.19,self.winsize_v*0.59,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+
+        self.Sensorsc['robot_battery'] = QtGui.QLabel(self)
+        self.Sensorsc['robot_battery'].setText("Bateria:")
+        self.Sensorsc['robot_battery'].setStyleSheet("font-size:30px; Arial")
+        self.Sensorsc['robot_battery'].setGeometry(QtCore.QRect(self.winsize_h*0.19,self.winsize_v*0.62,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+
+
         #Setting HR Image
 
         self.heart=QtGui.QLabel(self)
@@ -62,6 +109,24 @@ class TherapyWindow(QtGui.QMainWindow):
         posture = posture.scaled(self.winsize_h*0.04,self.winsize_h*0.04,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
         self.posture.setPixmap(posture)
 
+        #Setting play and stop Images
+
+        self.play=QtGui.QLabel(self)
+        self.play.setGeometry(QtCore.QRect(self.winsize_h*0.85,self.winsize_v*0.8,self.winsize_h*0.07,self.winsize_h*0.07))
+        play =QtGui.QPixmap("img/begin.PNG")
+        play = play.scaled(self.winsize_h*0.07,self.winsize_h*0.07,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.play.setPixmap(play)
+
+        self.stop=QtGui.QLabel(self)
+        self.stop.setGeometry(QtCore.QRect(self.winsize_h*0.75,self.winsize_v*0.8,self.winsize_h*0.07,self.winsize_h*0.07))
+        stop =QtGui.QPixmap("img/stop1.PNG")
+        stop = stop.scaled(self.winsize_h*0.07,self.winsize_h*0.07,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.stop.setPixmap(stop)
+
+
+
+
+
         self.posture_label=QtGui.QLabel(self)
         self.posture_label.setGeometry(QtCore.QRect(self.winsize_h*0.24,self.winsize_v*0.44,self.winsize_h*0.25,self.winsize_h*0.03))
         posture_label=QtGui.QPixmap("img/rect5562.PNG")
@@ -75,11 +140,21 @@ class TherapyWindow(QtGui.QMainWindow):
         self.Patient['name'].setStyleSheet("font-size:30px; Arial")
         self.Patient['name'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.90,self.winsize_h*0.25 ,self.winsize_h*0.02))
 
-        self.Patient = {}
         self.Patient['id'] = QtGui.QLabel(self)
         self.Patient['id'].setText("ID del paciente :")
         self.Patient['id'].setStyleSheet("font-size:30px; Arial")
         self.Patient['id'].setGeometry(QtCore.QRect(self.winsize_h*0.05,self.winsize_v*0.93,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        self.Patient['start'] = QtGui.QLabel(self)
+        self.Patient['start'].setText("Iniciar terapia")
+        self.Patient['start'].setStyleSheet("font-size:30px; Arial")
+        self.Patient['start'].setGeometry(QtCore.QRect(self.winsize_h*0.85,self.winsize_v*0.90,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
+        self.Patient['end'] = QtGui.QLabel(self)
+        self.Patient['end'].setText("Finalizar terapia")
+        self.Patient['end'].setStyleSheet("font-size:30px; Arial")
+        self.Patient['end'].setGeometry(QtCore.QRect(self.winsize_h*0.75,self.winsize_v*0.90,self.winsize_h*0.25 ,self.winsize_h*0.02))
+
 
         
         #Setting priority texts
@@ -123,7 +198,7 @@ class TherapyWindow(QtGui.QMainWindow):
         self.ECG['name'].setGeometry(QtCore.QRect(self.winsize_h*0.1,self.winsize_v*0.35,self.winsize_h*0.3 ,self.winsize_h*0.02))
 
         self.ECG['lcd'] = QtGui.QLCDNumber(self)
-        self.ECG['lcd'].setGeometry(QtCore.QRect(self.winsize_h*0.26,self.winsize_v*0.34,self.winsize_h*0.1 ,self.winsize_h*0.03))
+        self.ECG['lcd'].setGeometry(QtCore.QRect(self.winsize_h*0.24,self.winsize_v*0.34,self.winsize_h*0.168 ,self.winsize_h*0.03))
 
         self.Inclination = {}
         self.Inclination['name'] = QtGui.QLabel(self)
@@ -132,7 +207,7 @@ class TherapyWindow(QtGui.QMainWindow):
         self.Inclination['name'].setGeometry(QtCore.QRect(self.winsize_h*0.1,self.winsize_v*0.45,self.winsize_h*0.3 ,self.winsize_h*0.02))
 
         self.Inclination['lcd'] = QtGui.QLCDNumber(self)
-        self.Inclination['lcd'].setGeometry(QtCore.QRect(self.winsize_h*0.26,self.winsize_v*0.44,self.winsize_h*0.1 ,self.winsize_h*0.03))
+        self.Inclination['lcd'].setGeometry(QtCore.QRect(self.winsize_h*0.24,self.winsize_v*0.44,self.winsize_h*0.168,self.winsize_h*0.03))
 
         # Setting the buttons of the interface:
         self.controlButtons={}
@@ -141,11 +216,23 @@ class TherapyWindow(QtGui.QMainWindow):
         self.controlButtons['close'].setIconSize(QSize(0,0))
         self.controlButtons['close'].setGeometry(QtCore.QRect(self.winsize_h*0.93,self.winsize_v*0.05,self.winsize_h*0.045,self.winsize_h*0.045)) 
         
+        #Graphic Plot Widget
+
+        self.EMGDisplay={}
+        self.EMGDisplay['name'] = QtGui.QLabel(self)
+        self.EMGDisplay['name'].setText("Electromyography Graphic")
+        self.EMGDisplay['name'].setStyleSheet("font-size:35px; Arial")
+        self.EMGDisplay['name'].setGeometry(QtCore.QRect(self.winsize_h*0.66,self.winsize_v*0.1,self.winsize_h*0.2,self.winsize_h*0.1))
+        
+        
+
+        self.EMGDisplay['display']= PlotWidget(self)
+        self.EMGDisplay['display'].setGeometry(QtCore.QRect(self.winsize_h*0.58,self.winsize_v*0.2,self.winsize_h*0.3 ,self.winsize_h*0.3))
+        self.EMGDisplay['display'].plotItem.showGrid(True, True, 0.7)
 
 
        
         self.show()
-        self.setPatientName("Nathalia Cespedes Gomez", "1026295355")
 
         self.connectCloseButton();
         
@@ -157,7 +244,7 @@ class TherapyWindow(QtGui.QMainWindow):
         self.controlButtons['close'].clicked.connect(self.close)
         self.onClose.emit()
 
-    def setPatientName(self, value, id_patient):
+    def setInfoData(self, value, id_patient):
 
         value =str(value) 
         self.SetPatient = {}
@@ -173,6 +260,24 @@ class TherapyWindow(QtGui.QMainWindow):
         self.SetPatient['id'].setStyleSheet("font-size:30px; Arial")
         self.SetPatient['id'].setGeometry(QtCore.QRect(self.winsize_h*0.18,self.winsize_v*0.93,self.winsize_h*0.25 ,self.winsize_h*0.02))
         self.SetPatient['id'].show()
+
+    def sensorsData(self, ip, battery):
+
+        ip = str(ip)
+        self.SetRobot = {}
+        self.SetRobot['ip'] = QtGui.QLabel(self)
+        self.SetRobot['ip'].setText(ip)
+        self.SetRobot['ip'].setStyleSheet("font-size:30px; Arial")
+        self.SetRobot['ip'].setGeometry(QtCore.QRect(self.winsize_h*0.26,self.winsize_v*0.59,self.winsize_h*0.25 ,self.winsize_h*0.02))
+        self.SetRobot['ip'].show()
+
+        battery = str(battery)
+
+        self.SetRobot['battery'] = QtGui.QLabel(self)
+        self.SetRobot['battery'].setText(battery)
+        self.SetRobot['battery'].setStyleSheet("font-size:30px; Arial")
+        self.SetRobot['battery'].setGeometry(QtCore.QRect(self.winsize_h*0.26,self.winsize_v*0.62,self.winsize_h*0.25 ,self.winsize_h*0.02))
+        self.SetRobot['battery'].show()
 
 
 
